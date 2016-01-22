16255b95d80f9db35a66a335bd92677d5013ffc6
nbauers <nbauers@btinternet.com>
2016-01-22 10:57:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+Source Code View
 <?php
 // -------------------------------------------------------------------------------
 // This code should be run from a PHP enabled web server.
@@ -5,9 +6,18 @@
 // Here is a live example ...
 // http://nbest.co.uk/kmlGeohash/testForm.php
 // -------------------------------------------------------------------------------
-  //$debug = false;
-  $debug = true;
-  
+
+// -------------------------------------------------------------------------------
+  if ((isset($_GET)) && (isset($_GET['debug'])) && ($_GET['debug'] != ""))
+  {
+    $debug = true;
+  }
+  else
+  {
+    $debug = false;
+  }
+// -------------------------------------------------------------------------------
+
   if ($debug) 
   {
     echo "<!DOCTYPE HTML>\n";
@@ -17,7 +27,8 @@
     echo "  <title>KML Calculator - Debug</title>\n";
     echo "</head>\n";
     echo "<body>\n";
-    echo "<h2>The code is not working right now. Sorry!</h2>\n";
+    echo "<h2>Debug Mode</h2>\n";
+    echo "<hr>\n";
   }
 // -------------------------------------------------------------------------------
 
@@ -77,6 +88,7 @@
   if (! is_int($get_skins + 0))     $get_skins =    1;
   if ($get_skins > 6)               $get_skins =    6;
   if ($get_skins < 0)               $get_skins =    0;
+  if ($debug) echo"<p>\$get_skins $get_skins</p>\n";
   
   if ($get_lat == "")               $get_lat   =   51;
   if (! is_int($get_lat + 0))       $get_lat   =   51;
@@ -86,6 +98,7 @@
   if ($get_lat === "-0")            $get_lat   =   -1;               //                          ##
   if ($get_lat + $get_skins >   89) $get_lat   =   89 - $get_skins;  //  89  88  87  86  85  84  83  82  81  80  79  78  77
   if ($get_lat - $get_skins <  -89) $get_lat   =  -90 + $get_skins;  //  89  88  87  86  85  84  83  82  81  80  79  78  77
+  if ($debug) echo"<p>\$get_lat $get_lat</p>\n";
   
   if ($get_lon == "")               $get_lon   =    0;
   if (! is_int($get_lon + 0))       $get_lon   =    0;
@@ -95,16 +108,17 @@
   if ($get_lon === "-0")            $get_lon   =   -1;               //                         ###
   if ($get_lon + $get_skins >  179) $get_lon   =  179 - $get_skins;  // 179 178 177 176 175 174 173 172 171 170 169 168 167
   if ($get_lon - $get_skins < -179) $get_lon   = -180 + $get_skins;  // 179 178 177 176 175 174 173 172 171 170 169 168 167
+  if ($debug) echo"<p>\$get_lon $get_lon</p>\n";
   
-  if ($debug) echo "<p>Input: $get_date</p>\n";
+  if ($debug) echo "<p>Original \$get_date $get_date</p>\n";
   if ($get_date == "")              $get_date  = date("Y-m-d");      // Today is the default
-  if ($debug) echo "<p>$get_date</p>\n";
+  if ($debug) echo "<p>After blank test \$get_date $get_date</p>\n";
   if (! validateDate($get_date))
   {
     if ((is_int($get_date + 0)) && ($get_date >= -7) && ($get_date <= 7)) $get_date = tweekDate(date("Y-m-d"), $get_date); else $get_date = date("Y-m-d");
-    if ($debug) echo "<p>$get_date</p>\n";
+    if ($debug) echo "<p>Tweeked date \$get_date $get_date</p>\n";
   }
-  if ($debug) echo "<p>$get_date</p>\n";
+  if ($debug) echo "<p>Final \$get_date $get_date</p>\n";
 
 // -------------------------------------------------------------------------------
 // Attempt to get the DJIA
@@ -154,29 +168,33 @@
   // -----------------------------------------------------------------------------
   // Get data west of -30
   // -----------------------------------------------------------------------------
+  $dija_w_found = false;  // Fail Safe
+  $djia_w = -1;           // Can this ever happen in real life?
+  
   if(get_http_response_code("http://geo.crox.net/djia/$dow_date_w") != "200"){
-    echo "<h1>No valid DJIA found for $dow_date_w - Sorry!</h1>";
-    exit;
+    $dija_w_found = false;
   }
   else
   {
     $djia_w = file_get_contents("http://geo.crox.net/djia/$dow_date_w");
 
-    if (! is_numeric($djia_w))
+    if (is_numeric($djia_w))
+    {
+      $dija_w_found = true;
+    }
+    else
     {
       echo "<h1>DJIA \"$djia_w\" seems to be invalid - Sorry!</h1>";
-      exit;
+      $dija_w_found = false;
     }
   }
- 
-  // echo "<p>Debugging: $djia_e</p>\n";
 // -------------------------------------------------------------------------------
 // Get the fractional parts of lat and lon into array index 0 and 1
 // -------------------------------------------------------------------------------
   $calc = new calc();
-  
+
   list($lat_e, $lon_e) = $calc->doCalc($get_date, $djia_e);
-  list($lat_w, $lon_w) = $calc->doCalc($get_date, $djia_w);
+  list($lat_w, $lon_w) = $calc->doCalc($get_date, $djia_w);  // Result will not be valid if $dija_w_found is false
   
   if ($debug) echo "<p>\$djia_e = $djia_e<br>\$djia_w = $djia_w</p>\n";
   if ($debug) echo "<p>\$lat_e = $lat_e<br>\$lon_e = $lon_e</p><p>\$lat_w = $lat_w<br>\$lon_w = $lon_w</p>\n";
@@ -231,10 +249,6 @@
   {
     echo "<table style=\"border-collapse: collapse; border:solid 1px #bbb;\">\n";                    // Debug only
     echo "<tr>\n";                                                                                   // Debug only
-    echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$get_skins</td>\n"; // Debug only
-    echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$get_date</td>\n";  // Debug only
-    echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$get_lat</td>\n";   // Debug only
-    echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$get_lon</td>\n";   // Debug only
     echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$grat_lat</td>\n";  // Debug only
     echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$grat_lon</td>\n";  // Debug only
     echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$lat</td>\n";       // Debug only
@@ -244,99 +258,100 @@
 
   for ($yy_lat = -$get_skins; $yy_lat < ($get_skins + 1);  $yy_lat++) {      // Iterate through latitudes  (vertical)
     for ($xx_lon = -$get_skins; $xx_lon < ($get_skins + 1);  $xx_lon++) {    // Iterate through longitudes (horizontal)
-      // -------------------------------------------------------------------------
-      //  $yy_lat -3, -2, -1,  0,  1,  2,  3     Iterator Index
-      //          -2, -1, -0,  0,  1,  2,  3     Graticule name
-      // -------------------------------------------------------------------------
-      if ($get_lon + $xx_lon >= -30)   // Use $lat_e  and $lon_e
+	  if ((($get_lon + $xx_lon < -30) && ($dija_w_found)) || ($get_lon + $xx_lon >= -30))
       {
-        if ($get_lat + $yy_lat >= 0)   // North of the equator
-        { 
-          $lat = $get_lat + $yy_lat + $lat_e;
-		}
-		else
-		{
-          $lat = 1 + $get_lat + $yy_lat - $lat_e;
-		}
+        // -------------------------------------------------------------------------
+        //  $yy_lat -3, -2, -1,  0,  1,  2,  3     Iterator Index
+        //          -2, -1, -0,  0,  1,  2,  3     Graticule name
+        // -------------------------------------------------------------------------
+        if ($get_lon + $xx_lon >= -30)   // Use $lat_e  and $lon_e
+        {
+          if ($get_lat + $yy_lat >= 0)   // North of the equator
+          { 
+            $lat = $get_lat + $yy_lat + $lat_e;
+          }
+          else
+          {
+            $lat = 1 + $get_lat + $yy_lat - $lat_e;
+          }
 
-        if ($get_lon + $xx_lon >= 0)   // East of the meridian
-		{
-		  $lon = $get_lon + $xx_lon + $lon_e;
-		}
-		else
-		{
-		  $lon = 1 + $get_lon + $xx_lon - $lon_e;
-		}
-	  }
-	  else                             // Use $lat_w  and $lon_w
-	  {
-        if ($get_lat + $yy_lat >= 0)   // North of the equator
-        { 
-          $lat = $get_lat + $yy_lat + $lat_w;
-		}
-		else
-		{
-          $lat = 1 + $get_lat + $yy_lat - $lat_w;
-		}
+          if ($get_lon + $xx_lon >= 0)   // East of the meridian
+          {
+            $lon = $get_lon + $xx_lon + $lon_e;
+          }
+          else
+          {
+            $lon = 1 + $get_lon + $xx_lon - $lon_e;
+          }
+        }
+        else                             // Use $lat_w  and $lon_w
+        {
+          if ($get_lat + $yy_lat >= 0)   // North of the equator
+          { 
+            $lat = $get_lat + $yy_lat + $lat_w;
+          }
+          else
+          {
+            $lat = 1 + $get_lat + $yy_lat - $lat_w;
+          }
 
-        if ($get_lon + $xx_lon >= 0)   // East of the meridian
-		{
-		  $lon = $get_lon + $xx_lon + $lon_w;
-		}
-		else
-		{
-		  $lon = 1 + $get_lon + $xx_lon - $lon_w;
-		}
-	  }
-      // -------------------------------------------------------------------------
-      if ($get_lat + $yy_lat >=  0) { $grat_lat = $get_lat + $yy_lat; } else { $grat_lat  = 1 + $get_lat + $yy_lat; }
-      if ($get_lat + $yy_lat == -1) { $grat_lat = "-" . $grat_lat; }
+          if ($get_lon + $xx_lon >= 0)   // East of the meridian
+          {
+            $lon = $get_lon + $xx_lon + $lon_w;
+          }
+          else
+          {
+            $lon = 1 + $get_lon + $xx_lon - $lon_w;
+          }
+        }
+        // -------------------------------------------------------------------------
+        if ($get_lat + $yy_lat >=  0) { $grat_lat = $get_lat + $yy_lat; } else { $grat_lat  = 1 + $get_lat + $yy_lat; }
+        if ($get_lat + $yy_lat == -1) { $grat_lat = "-" . $grat_lat; }
       
-      if ($get_lon + $xx_lon >=  0) { $grat_lon = $get_lon + $xx_lon; } else { $grat_lon  = 1 + $get_lon + $xx_lon; }
-      if ($get_lon + $xx_lon == -1) { $grat_lon = "-" . $grat_lon; }
-      // -------------------------------------------------------------------------
+        if ($get_lon + $xx_lon >=  0) { $grat_lon = $get_lon + $xx_lon; } else { $grat_lon  = 1 + $get_lon + $xx_lon; }
+        if ($get_lon + $xx_lon == -1) { $grat_lon = "-" . $grat_lon; }
+        // -------------------------------------------------------------------------
       
-      if ($debug) 
-      {
-        echo "<tr>\n";                                                                                  // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$get_skins</td>\n"; // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$get_date</td>\n";  // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$get_lat</td>\n";   // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$get_lon</td>\n";   // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lat</td>\n";  // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lon</td>\n";  // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lat</td>\n";       // Debug only
-        echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lon</td>\n";       // Debug only
-        echo "<tr>\n";                                                                                  // Debug only
+        if ($debug) 
+        {
+          echo "<tr>\n";                                                                                  // Debug only
+          echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lat</td>\n";  // Debug only
+          echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lon</td>\n";  // Debug only
+          echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lat</td>\n";       // Debug only
+          echo "<td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lon</td>\n";       // Debug only
+          echo "<tr>\n";                                                                                  // Debug only
+        }
+
+        $kml .= "    <Placemark>\n";
+        $kml .= "        <name>" . $grat_lat . " " . $grat_lon . " " . $day . "</name>\n";
+        $kml .= "           <description><![CDATA[" . 
+                              number_format($lat, 6) . " " . number_format($lon, 6) . "<br>" .
+                              "<a href=\"http://wiki.xkcd.com/geohashing/" . $get_date . "_" . $grat_lat . "_" . $grat_lon . "\">" . $get_date . " " . $grat_lat . " " .  $grat_lon . "</a><br>" . 
+                              "<a href=\"http://geo.crox.net/poster/" . $get_date . " " . $grat_lat . " " . $grat_lon . "\">Poster</a><br>" . 
+                              "<a href=\"http://wiki.xkcd.com/geohashing/" . $grat_lat . "," . $grat_lon . "\">Graticule</a><br>" . 
+                              "<a href=\"http://geohashing.info/" . $get_date . "/s/z:8/" . $grat_lat . "," . $grat_lon . "\">geohashing.info</a><br>" . 
+                              "<a href=\"http://carabiner.peeron.com/xkcd/map/map.html?date=" . $get_date . "&lat=" . $grat_lat . "&long=" . $grat_lon . "&zoom=8\">Peeron</a><br>" . 
+                              "<a href=\"http://www.openstreetmap.org/?mlat=" . $lat . "&mlon=" . $lon . "&zoom=16\">OSM</a><br>" . 
+                              "<a href=\"http://maps.google.com/?ie=UTF8&ll=" . $lat . "," . $lon . "&z=8&q=loc:" . $lat . "," . $lon . "\">Google Map</a><br>" . 
+                              "<a href=\"http://bing.com/maps/default.aspx?cp=" . $lat . "~" . $lon . "&lvl=15\">Bing Map</a><br>" . 
+                              "]]></description>\n";
+        $kml .= "        <LookAt>\n";
+        $kml .= "            <longitude>" . $lon . "</longitude>\n";
+        $kml .= "            <latitude>" . $lat . "</latitude>\n";
+        $kml .= "            <altitude>0</altitude>\n";
+        $kml .= "            <heading>0</heading>\n";
+        $kml .= "            <tilt>0</tilt>\n";
+        $kml .= "            <range>1000</range>\n";
+      //$kml .= "            <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>\n";
+        $kml .= "        </LookAt>\n";
+        $kml .= "        <styleUrl>#m_ylw-pushpin</styleUrl>\n";
+        $kml .= "        <Point>\n";
+        $kml .= "            <gx:drawOrder>1</gx:drawOrder>\n";
+        $kml .= "            <coordinates>" . $lon . "," . $lat . ",0</coordinates>\n";
+        $kml .= "        </Point>\n";
+        $kml .= "    </Placemark>\n";
       }
-
-      $kml .= "    <Placemark>\n";
-      $kml .= "        <name>" . $grat_lat . " " . $grat_lon . " " . $day . "</name>\n";
-      $kml .= "           <description><![CDATA[" . 
-                            number_format($lat, 8) . " " . number_format($lon, 8) . "<br>" .
-                            "<a href=\"http://wiki.xkcd.com/geohashing/" . $get_date . "_" . $grat_lat . "_" . $grat_lon . "\">" . $get_date . " " . $grat_lat . " " .  $grat_lon . "</a><br>" . 
-                            "<a href=\"http://geo.crox.net/poster/" . $get_date . " " . $grat_lat . " " . $grat_lon . "\">Poster</a><br>" . 
-                            "<a href=\"http://wiki.xkcd.com/geohashing/" . $grat_lat . "," . $grat_lon . "\">Graticule</a><br>" . 
-                            "<a href=\"http://geohashing.info/" . $get_date . "/s/z:8/" . $grat_lat . "," . $grat_lon . "\">geohashing.info</a><br>" . 
-                            "<a href=\"http://carabiner.peeron.com/xkcd/map/map.html?date=" . $get_date . "&lat=" . $grat_lat . "&long=" . $grat_lon . "&zoom=8\">Peeron</a><br>" . 
-                            "<a href=\"http://www.openstreetmap.org/?mlat=" . $lat . "&mlon=" . $lon . "&zoom=16\">OSM</a><br>" . 
-                            "<a href=\"http://maps.google.com/?ie=UTF8&ll=" . $lat . "," . $lon . "&z=8&q=loc:" . $lat . "," . $lon . "\">Google Map</a>]]></description>\n";
-      $kml .= "        <LookAt>\n";
-      $kml .= "            <longitude>" . $lon . "</longitude>\n";
-      $kml .= "            <latitude>" . $lat . "</latitude>\n";
-      $kml .= "            <altitude>0</altitude>\n";
-      $kml .= "            <heading>0</heading>\n";
-      $kml .= "            <tilt>0</tilt>\n";
-      $kml .= "            <range>1000</range>\n";
-    //$kml .= "            <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>\n";
-      $kml .= "        </LookAt>\n";
-      $kml .= "        <styleUrl>#m_ylw-pushpin</styleUrl>\n";
-      $kml .= "        <Point>\n";
-      $kml .= "            <gx:drawOrder>1</gx:drawOrder>\n";
-      $kml .= "            <coordinates>" . $lon . "," . $lat . ",0</coordinates>\n";
-      $kml .= "        </Point>\n";
-      $kml .= "    </Placemark>\n";
-    }
+	}    // if (((if ($get_lon + $xx_lon < -30) && ($dija_w_found)) || (if ($get_lon + $xx_lon >= 30))
   }  
   
   if ($debug) echo "</table>\n";
