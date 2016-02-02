cb26da8276ccdd7dd24fd9a273c9daf20b5d2fc0
nbauers <nbauers@btinternet.com>
2016-02-02 16:34:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,289 +1,110 @@
-<?php
-// -----------------------------------------------------------------------------------------
-// This code should be run from a PHP enabled web server.
-// Call up the page with your choice of date, latitude, longitude, skins and debug mode
-//
-// There is an input form ...
-// http://nbest.co.uk/kmlGeohash/testForm.php
-//
-// Or call the calculator page directly - all the parameters have default values if omitted
-// http://nbest.co.uk/kmlGeohash/index.php?date=2015-12-12&lat=52&lon=-0&skins=2&debug=debug
-// -----------------------------------------------------------------------------------------
-  require_once("hash_up.php");     // Geohashing functions
-  require_once("html_gen.php");    // html functions
-  require_once("kml_gen.php");     // KML functions
-// -----------------------------------------------------------------------------------------
-  $djia_w = false;
-  $djia_w = false;
-// -----------------------------------------------------------------------------------------
-  
-  // ---------------------------------------------------------------------------------------
-  // Validate $_GET
-  // ---------------------------------------------------------------------------------------
-  $clean_get = clean_up_get_params($_GET);
-  extract($clean_get);
-  // ---------------------------------------------------------------------------------------
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
 
-  // ---------------------------------------------------------------------------------------
-  if ($get_debug)
-  {
-    html_head();    
-    echo "<h2>Debug Mode</h2>\n";
-    echo "<hr>\n";
-    echo "<pre>\$_GET\n"      . print_r($_GET, true)      . "</pre>\n";
-    echo "<pre>\$clean_get\n" . print_r($clean_get, true) . "</pre>\n";
-    echo "<p>\$get_date $get_date<br>\$get_lat $get_lat<br>\$get_lon $get_lon<br>\$get_skins $get_skins<br>\$get_debug $get_debug</p>\n";
-  }    
-  
-  // ---------------------------------------------------------------------------------------
-  // Get djia values for date and day before - could be null
-  // ---------------------------------------------------------------------------------------
-  list($djia_e, $djia_w, $msg) = get_djias($get_date, $get_debug);
-  // ---------------------------------------------------------------------------------------
-  
-  if ($get_debug)
-  {
-    echo "<p>";
-    if ($djia_e) echo "\$djia_e = $djia_e<br>\n";
-    if ($djia_w) echo "\$djia_w = $djia_w<br>\n";
-    echo "\$msg<br>$msg"; 
-    echo "</p>\n";
-  }
-  
-  if (($djia_w === false) && ($djia_e === false))  // If both djia values are missing ...
-  {
-    if ($get_debug)
-    {
-      echo "<h3>$msg</h3>";
+  <title>Geohashing KML Calculator</title>
+  <link href="testForm.css" rel="stylesheet" type="text/css">
+<script>
+  // -------------------------------------------------------------
+  // http://nbest.co.uk/kmlGeohash/index.php?date=2016-01-01&lat=52&lon=0&skins=1
+  function urlGen() {
+    var f_date  = document.forms["kmlForm"]["f_date"].value;
+    var f_lat   = document.forms["kmlForm"]["f_lat"].value;
+    var f_lon   = document.forms["kmlForm"]["f_lon"].value;
+    var f_clat  = document.forms["kmlForm"]["f_clat"].value;
+    var f_clon  = document.forms["kmlForm"]["f_clon"].value;
+    var f_skins = document.forms["kmlForm"]["f_skins"].value;
+    var f_debug = "";
+
+    if (document.forms["kmlForm"]["f_debug"].checked){
+      f_debug = "debug";
+    }else{
+      f_debug = "";
     }
-    else
-    {
-      html_head();
-      echo "<h3>$msg</h3>\n\n";
-      html_tail();
-      
-      exit;
-    }
-  }
-  
-  // ---------------------------------------------------------------------------------------
-  // Get coordinates for east and west of 30W
-  // ---------------------------------------------------------------------------------------
-  if ($djia_e) list($lat_e, $lon_e) = get_coords($get_date, $djia_e, $get_debug);
-  if ($djia_e) list($lat_g, $lon_g) = get_global($get_date, $djia_e, $get_debug);
-  if ($djia_w) list($lat_w, $lon_w) = get_coords($get_date, $djia_w, $get_debug);
-  // ---------------------------------------------------------------------------------------
-  
-  if ($get_debug)
-  {
-    echo "<p>";
-    if ($djia_e) echo "\$lat_e = $lat_e \$lon_e = $lon_e";
-    if ($djia_w) echo "<br>\$lat_w = $lat_w \$lon_w = $lon_w<br><br>\$lat_g = $lat_g \$lon_g = $lon_g";
-    echo "</p>\n\n";
-  }
-
-  $day    = date('D', strtotime($get_date));      // Get "Mon"    or something similar from 2016-01-25
-  $day_nn = $day . " " . substr ($get_date, 8 );  // Get "Mon 25" or something similar from 2016-01-25
-  
-  $kml       = "";
-  $countPins = 0;
-  
-  if ($djia_e || $djia_w)    // Do the nested for loops ...
-  {
-    if ($get_debug)
-    {
-      echo "<table style=\"border-collapse: collapse; border:solid 1px #bbb;\">\n";
-      echo "  <tr>\n";
-      echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$grat_lat</td>\n";
-      echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$grat_lon</td>\n";
-      echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$lat</td>\n";
-      echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">\$lon</td>\n";
-      echo "  <tr>\n";
-    }
-
- // $kml_head  = kml_begin($get_date . "_$day.kml", $lat, $lon);    // It's more convenient to get this inside the nested loop below
-    $kml      .= kml_style();                                       // push pin and styles
-
-    $min_lat =   90;
-    $min_lon =  180;
-    $max_lat =  -90;
-    $max_lon = -180;
-
-    // -------------------------------------------------------------------------------------
-    // Generate the push pins
-    // -------------------------------------------------------------------------------------
-    $count = 0;
-    $kml .= kml_folder_start("Pins");
-    for ($yy_lat = -$get_skins; $yy_lat < ($get_skins + 1);  $yy_lat++) {      // Iterate through latitudes  (vertical)
-      for ($xx_lon = -$get_skins; $xx_lon < ($get_skins + 1);  $xx_lon++) {    // Iterate through longitudes (horizontal)
-        if ((($get_lon + $xx_lon < -30) && ($djia_w)) || ($get_lon + $xx_lon >= -30))
-        {
-          // -------------------------------------------------------------------------
-          //  $yy_lat -3, -2, -1,  0,  1,  2,  3     Iterator Index
-          //          -2, -1, -0,  0,  1,  2,  3     Graticule name
-          // -------------------------------------------------------------------------
-          if ($get_lon + $xx_lon >= -30)   // Use $lat_e  and $lon_e
-          {
-            if ($get_lat + $yy_lat >= 0)   // North of the equator
-            { 
-              $lat = $get_lat + $yy_lat + $lat_e;
-            }
-            else
-            {
-              $lat = 1 + $get_lat + $yy_lat - $lat_e;
-            }
-  
-            if ($get_lon + $xx_lon >= 0)   // East of the meridian
-            {
-              $lon = $get_lon + $xx_lon + $lon_e;
-            }
-            else
-            {
-              $lon = 1 + $get_lon + $xx_lon - $lon_e;
-            }
-          }
-          else                             // Use $lat_w  and $lon_w
-          {
-            if ($get_lat + $yy_lat >= 0)   // North of the equator
-            { 
-              $lat = $get_lat + $yy_lat + $lat_w;
-            }
-            else
-            {
-              $lat = 1 + $get_lat + $yy_lat - $lat_w;
-            }
-  
-            if ($get_lon + $xx_lon >= 0)   // East of the meridian
-            {
-              $lon = $get_lon + $xx_lon + $lon_w;
-            }
-            else
-            {
-              $lon = 1 + $get_lon + $xx_lon - $lon_w;
-            }
-          }
-          // -------------------------------------------------------------------------
-          if ($get_lat + $yy_lat >=  0) { $grat_lat = $get_lat + $yy_lat; } else { $grat_lat  = 1 + $get_lat + $yy_lat; }
-          if ($get_lat + $yy_lat == -1) { $grat_lat = "-" . $grat_lat; }
-      
-          if ($get_lon + $xx_lon >=  0) { $grat_lon = $get_lon + $xx_lon; } else { $grat_lon  = 1 + $get_lon + $xx_lon; }
-          if ($get_lon + $xx_lon == -1) { $grat_lon = "-" . $grat_lon; }
-          // -------------------------------------------------------------------------
-        
-          if ($get_debug)
-          {
-            echo "  <tr>\n";
-            echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lat</td>\n";
-            echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$grat_lon</td>\n";
-            echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lat</td>\n";
-            echo "    <td style=\"border-style:solid; border:solid 1px #bbb; padding:2px;\">$lon</td>\n";
-            echo "  <tr>\n";
-          }
-
-          if (($yy_lat == 0) && ($xx_lon == 0))
-		  {
-		    if (($get_clat != "") && ($get_clon != ""))
-			{
-			  $kml_head = kml_begin($get_date . "_$day.kml", $get_clat, $get_clon, 150000);  // kml head section
-			}
-			else
-			{
-			  $kml_head = kml_begin($get_date . "_$day.kml", $lat, $lon, 1000);              // kml head section
-			}
-		  }
-
-          $kml .= kml_placemark($get_date, $grat_lat, $grat_lon, $lat, $lon, $day_nn);                       // kml placemark
-          $countPins++;
-
-          // -------------------------------------------------------------------------------
-          // mnn: Convert -0 notation to useable coordinates
-          //      1 => 1    0 => 0    -0 => -1    -1 => -2   etc.
-          // -------------------------------------------------------------------------------
-          if (mnn($grat_lat) <= $min_lat) $min_lat = mnn($grat_lat);    // -2  -1  -0   0   1   2   3
-          if (mnn($grat_lon) <= $min_lon) $min_lon = mnn($grat_lon);    // -3  -2  -1   0   1   2   3
-          if (mnn($grat_lat) >= $max_lat) $max_lat = mnn($grat_lat);
-          if (mnn($grat_lon) >= $max_lon) $max_lon = mnn($grat_lon);
-          if ($get_debug) echo "! " . mnn($grat_lat) . " " . mnn($grat_lon) . "<br>";
-          // -------------------------------------------------------------------------------
-
-          if ($count++ > 200) break;    // Discourage infinite loops!
-        }    // if ((($get_lon + $xx_lon < -30) && ($dija_w_found)) || ($get_lon + $xx_lon >= -30))
-      }      // for ($xx_lon = -$get_skins; $xx_lon < ($get_skins + 1);  $xx_lon++) {    // Iterate through longitudes (horizontal)
-    }        // for ($yy_lat = -$get_skins; $yy_lat < ($get_skins + 1);  $yy_lat++) {    // Iterate through latitudes  (vertical)
-    // -------------------------------------------------------------------------------------
-
-    // -------------------------------------------------------------------------------------
-    // GLOBAL HASH
-    // -------------------------------------------------------------------------------------
-    if ($djia_e)
-    {
-      $kml .= kml_globalmark($get_date, $lat_g, $lon_g, $day_nn);    // kml global placemark
-    }
-    // -------------------------------------------------------------------------------------
     
-    $kml .= kml_folder_end();
-
-    // -------------------------------------------------------------------------------------
-
-    // -------------------------------------------------------------------------------------
-    // Draw the grid lines
-    // -------------------------------------------------------------------------------------
-      if ($get_debug) echo "<p>\$min_lat $min_lat<br>" .
-                            "\$min_lon $min_lon<br>" .
-                            "\$max_lat $max_lat<br>" .
-                            "\$max_lon $max_lon</p>";
-    $count = 0;
-    $kml .= kml_folder_start("Grid");
-    for ($yy = $min_lat; $yy < $max_lat + 2;  $yy++)      // Iterate through latitudes (vertical)
-    {
-      for ($xx = $min_lon; $xx < $max_lon + 2;  $xx++)    // Iterate through longitudes (horizontal)
-      {
-        if ($xx < $max_lon + 1)
-        {
-          $kml .= kml_grid($xx, $yy, $xx + 1, $yy, $count);    // kml horizontal grid line (lon1,lat1,lon2,lat2)
-          $count++;
-        }
-        if ($yy < $max_lat + 1)
-        {
-          $kml .= kml_grid($xx, $yy, $xx, $yy + 1, $count);    // kml vertical   grid line (lon1,lat1,lon2,lat2)
-          $count++;
-        }
-        if ($get_debug) echo "<p>\$xx $xx, \$yy $yy</p>";
-        if ($count > 400) break;    // Discourage infinite loops!
-      }
-    }
-    // -------------------------------------------------------------------------------------
-
-    $kml .= kml_folder_end();
-
-    $kml .= kml_end();         // kml tail section
-    $kml  = $kml_head . $kml;  // prepend the head section
+    var f_url = "<pre><a href=\"http://nbest.co.uk/kmlGeohash/index.php?date=" + f_date;
+        f_url = f_url + "&lat=" + f_lat;
+        f_url = f_url + "&lon=" + f_lon;
+        f_url = f_url + "&clat=" + f_clat;
+        f_url = f_url + "&clon=" + f_clon;
+        f_url = f_url + "&skins=" + f_skins;
+        f_url = f_url + "&debug=" + f_debug;
+        f_url = f_url + "\">http://nbest.co.uk/kmlGeohash/index.php?date=" + f_date;
+        f_url = f_url + "&lat=" + f_lat;
+        f_url = f_url + "&lon=" + f_lon ;
+        f_url = f_url + "&clat=" + f_clat;
+        f_url = f_url + "&clon=" + f_clon;
+        f_url = f_url + "&skins=" + f_skins;
+        f_url = f_url + "&debug=" + f_debug;
+        f_url = f_url + "</a></pre>";
     
-    if ($get_debug) echo "</table>\n\n";
+    // console.log(f_url);
+    
+    document.getElementById("f_url").innerHTML = f_url;
   }
+  // -------------------------------------------------------------
+</script>
+</head>
 
-  // ---------------------------------------------------------------------------------------
-  if ($get_debug)
-  {
-    echo "<pre>KML Data:\n\n" . str_replace("<", "&lt;", $kml) . "</pre>\n\n";
-    html_tail();
-  }
-  else
-  {
-    if ($countPins > 0)
-    {
-      header('Content-type: application/vnd.google-earth.kml+xml');
-      header('Content-Disposition: attachment; filename="$get_date.kml"');
-      echo $kml;
-    }
-    else
-    {
-      html_head();
-      echo "<h3>30W zone - DJIA not available yet.</h3>\n\n";
-      html_tail();
-    }
-  }
-  // ---------------------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------------------
-?>
+<body>
+<h1> Geohashing KML Calculator</h1>
+<p><img src="Sourcerer%20KML.jpg" alt="KML Calculator" width="498" height="320" /></p>
+<ul>
+  <li>This tool works in conjunction with KML aware applications like  <a href="http://www.google.com/earth/">Google Earth</a> and <a href="https://marble.kde.org/">Marble</a>.</li>
+  <li>When you submit the form, if your PC is correctly set up, Google Earth will launch and show the calculated hashpoints.</li>
+  <li>This seems to work on Android mobile phones and perhaps others too.</li>
+  <li>UK geohashers might  find the link to the Ordnance Survey maps particularly useful. Outside the UK you just get the normal Bing maps.</li>
+  <li>Use this form or make a bookmark or shortcut - like this<br>
+    <a href="http://nbest.co.uk/kmlGeohash/index.php?date=2016-01-31&lat=52&lon=-1&clat=52.5&clon=1.5&skins=3">http://nbest.co.uk/kmlGeohash/index.php?date=2016-01-31&lat=52&lon=-1&clat=52.5&clon=1.5&skins=3</a>
+  </li>
+  <li>For the <strong>Globalhash</strong>, zoom out until it comes into view.</li>
+</ul>
+<form action="index.php" id="kmlForm">
+<table>
+  <tr>
+    <td style="text-align:center;"><strong>date</strong></td>
+    <td><input name="date" type="text" id="f_date" size="12" maxlength="10" onKeyUp="urlGen()"></td>
+    <td><strong>Date: yyyy-mm-dd</strong> - if blank, the date defaults to today - alternatively use -7 to 7,  1 means tomorrow, -2 is the day before yesterday.</td>
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>lat</strong></td>
+    <td><input name="lat" type="text" id="f_lat" size="12" maxlength="4" onKeyUp="urlGen()"></td>
+    <td><strong>Latitude: -89 to 89</strong> - if blank, the latitude defaults to  51 (Grenwich). Whole numbers please.</td>
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>lon</strong></td>
+    <td><input name="lon" type="text" id="f_lon" size="12" maxlength="4" onKeyUp="urlGen()"></td>
+    <td><strong>Longitude: -179 to 179</strong> - if blank, the longitude defaults to  0 (Grenwich meridian). Whole numbers please.</td>
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>clat</strong></td>
+    <td><input name="clat" type="text" id="f_clat" size="12" maxlength="4" onKeyUp="urlGen()"></td>
+    <td><strong>Centre Latitude: -89 to 89</strong> - optional view centre. It's OK to leave this blank.
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>clon</strong></td>
+    <td><input name="clon" type="text" id="f_clon" size="12" maxlength="4" onKeyUp="urlGen()"></td>
+    <td><strong>Centre Longitude: -179 to 179</strong> -  - optional view centre. It's OK to leave this blank.
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>skins</strong></td>
+    <td><input name="skins" type="text" id="f_skins" size="12" maxlength="1" onKeyUp="urlGen()"></td>
+    <td><strong>Skins: 0 to 6.</strong> The default is 1. This gives 9 hashpoints. 6 skins will give you 169 hashpoints.</td>
+  </tr>
+  <tr>
+    <td style="text-align:center;"><strong>debug</strong></td>
+    <td style="text-align:center;"><input name="debug" type="checkbox" id="f_debug" value="debug" onClick="urlGen()"></td>
+    <td><strong>Debug:</strong> Check this to get debuging information. The KML data will be shown as text.</td>
+  </tr>
+  <tr>
+    <td> </td>
+    <td style="text-align:center;"><p><input type="submit" name="Submit" id="Submit" value="Submit"></p></td>
+    <td><span id="f_url"></spanp></td>
+  </tr>
+</table>
+</form>
+<p><strong>Disclaimer:</strong> Do no damage. Don't disturb people, animals or the environment. Stay safe!</p>
+<p><a href="http://wiki.xkcd.com/geohashing/User:Sourcerer/KML_tool">Geohashing Wiki</a> - <a href="https://github.com/nbauers/Geohashing-KML-Calculator">Code on GitHub</a> - <a href="source.php">Calculator Live Source</a></p>
+</body>
+</html>
